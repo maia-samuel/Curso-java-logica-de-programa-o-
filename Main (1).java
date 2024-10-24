3adc6df426bcc4147970ae7b299517718c503ced
Samuel Maia <89760100+shasmuel@users.noreply.github.com>
2024-10-24 16:30:59 -0300

    Add files via upload

---
+++ Main (1).java
@@ -0,0 +1,33 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+
+
+    Scanner sc = new Scanner(System.in);
+    
+    double  a , b , pi , c ;
+    
+    pi = 3.14159;
+    a = sc.nextDouble();
+    b = Math.pow(a,2);
+    c = b*pi;
+    
+    System.out.printf("sua area é %.3f", c);
+    
+    sc.close();
+
+
+
+}
+	
+	
+	
+}
