9b83f8ec85fbc2c1109a74e8bd856ece6aefd20c
Samuel Maia <89760100+maia-samuel@users.noreply.github.com>
2024-11-14 14:02:09 -0300

    Add files via upload

---
+++ Main (12).java
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);	    
+    int n = sc.nextInt();
+    int fatorial = 1 ;
+    for (int x = 1; x <=n; x++){
+        fatorial *= x;
+    }
+	    System.out.println(fatorial);
+	    
+	    sc.close();
+	}
+}
